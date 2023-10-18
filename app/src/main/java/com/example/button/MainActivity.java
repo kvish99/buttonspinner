8115dbb02be8941442f3a8d5f81bcc80873fb9b3
kvish99 <147805683+kvish99@users.noreply.github.com>
2023-10-18 15:43:37 +0530

    spinner

---
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+   CheckBox checkbox;
+
+   RadioButton rb_gender;
+
+    Spinner spinner;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkbox=findViewById(R.id.checkbox);
+        RadioButton=findViewById(R.id.rb_)
+
+
+        //checkbox
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
